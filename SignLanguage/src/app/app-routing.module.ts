3a0eaf044b0b2c13a95561dc14b7efb78b722a7a
Dialvive <dialvive@protonmail.com>
2021-02-04 08:41:48 -0600

    Words & Phrases OK

--- SignLanguage/src/app/app-routing.module.ts
+++ SignLanguage/src/app/app-routing.module.ts
@@ -1,11 +1,15 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { PhraseComponent } from './_components/phrase/phrase.component';
 import { ResultsComponent } from './_components/results/results.component';
 import { SearchComponent } from './_components/search/search.component';
+import { WordComponent } from './_components/word/word.component';
 
 const routes: Routes = [
   { path: '', component:  SearchComponent },
   { path: 'search', component:  ResultsComponent },
+  { path: 'word', component: WordComponent},
+  { path: 'phrase', component: PhraseComponent},
 ];
 
 @NgModule({
