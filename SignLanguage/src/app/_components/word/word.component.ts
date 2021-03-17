4d7fb40c299e2ef96d2987cc3d4a2d3913e0fd65
dialvive <dialvive@protonmail.com>
2021-03-17 08:52:12 -0600

    Modularized and cleaned code, added sign version feature and works ok

--- SignLanguage/src/app/_components/word/word.component.ts
+++ SignLanguage/src/app/_components/word/word.component.ts
@@ -5,8 +5,9 @@ import { Router } from '@angular/router';
 import { Word } from '../../_models/word';
 import { WordService } from '../../_services/word.service';
 import { HttpClient } from '@angular/common/http';
-import { map } from 'rxjs/operators';
-import Bootstrap from 'bootstrap/dist/js/bootstrap';
+import { delayWhen, map } from 'rxjs/operators';
+import { WordSign } from 'src/app/_models/wordSign';
+import { WordSignService } from 'src/app/_services/word-sign.service';
 
 @Component({
   selector: 'app-word',
@@ -15,12 +16,18 @@ import Bootstrap from 'bootstrap/dist/js/bootstrap';
 })
 export class WordComponent implements OnInit {
 
-  vid: any;
-  carousel: Bootstrap.Carousel;
-  private locale: string[] | any;   // Locale is expected to have three values: [0]spokenLang [1]signLang [2]country
   public word: Observable<Word> | any;
+  public videos: String[] | any;
+
   public wordID: string | any;
-  public localeInt: number | any;
+  public wordTXT: string | any;
+  private locale: string[] | any; // Locale is expected to have three values: [0]spokenLang [1]signLang [2]country
+  public localeInt: number | any; // 0: de, 1: es, 2: en, 3: fr, 4: it, 5: pt
+  public ready: boolean = false;
+  public error: boolean = false;
+
+  vid: any;
+
   public strDef: string[] = ["Definition", "Definición", "Definition", "Définition", "Definizione", "Definição"];
   public strExp: string[] = ["Erläuterung", "Explicación", "Explanation", "Explication","Spiegazione", "Explicação"];
   public strDet: string[] = ["Einzelheiten", "Detalles", "Details", "Détails", "Dettagli", "Detalhes"];
@@ -34,34 +41,29 @@ export class WordComponent implements OnInit {
     private route: ActivatedRoute,
     private router: Router,
     private wordService: WordService,
+    private wordSignService: WordSignService,
     private http: HttpClient,
   ) { }
 
   ngOnInit(): void {
-    this.vid = document.getElementById("sign-video");
-    this.carousel = document.getElementById('video-carousel')
-
     this.getLocale()
+    this.getIdTxt()
+    this.getWord();
+    this.createVideoURLs()
+    this.vid = document.getElementsByName("sign-video");
+    //TODO: Add a way to trigger the video loading after everything has been loaded.
+    //TODO: Create event listener for videos or carousel to load only the video on focus on the carousel.
 
-    const wordTxt: string | null = this.route.snapshot.queryParamMap.get('txt')
-    this.wordID =  this.route.snapshot.queryParamMap.get('id')
-    
-    if (this.wordID == null && wordTxt == null) {
-      this.navigate("/404")
-    } else if (wordTxt != null && this.wordID == null) {
-      this.navigate("/search")
-    }
 
-    // TODO: What if txt doesn't match with current locale txt?
-   this.wordService.getWord(+this.wordID).subscribe(
-    response => {
-      this.word = new Word(response);
-    }, 
-    err => console.error(err))
+
+    // TODO: What if txt doesn't match current locale txt?
+
+
   }
 
-  //Gets locale through params, or infers it using navigator or IP address
-  getLocale() {
+  //Gets locale through params, or infers it using navigator or IP address.
+  // TODO: infer sign language.
+  private getLocale(): void {
     var country: string | any;
     var loc = this.route.snapshot.queryParamMap.get('loc')
     if (loc == null) {
@@ -85,6 +87,7 @@ export class WordComponent implements OnInit {
     this.localeInt = this.getLocaleInt(this.locale[0])
   }
 
+  //Sets the LocaleInt globally depending on a given alpha2 country code.
   public getLocaleInt(str: string): number {
     switch (str) {
       case 'de':
@@ -104,8 +107,57 @@ export class WordComponent implements OnInit {
     }
   }
 
+  //Gets the id and txt parameters from the URL and instanciates it globally.
+  private getIdTxt(): void {
+    this.wordTXT = this.route.snapshot.queryParamMap.get('txt');
+    this.wordID =  this.route.snapshot.queryParamMap.get('id');
+
+    if (this.wordID == null && this.wordTXT == null) {
+      this.navigate("/404", this.locale, this.wordID, this.wordTXT);
+    } else if (this.wordTXT != null && this.wordID == null) {
+      this.navigate("/search", this.locale, this.wordID, this.wordTXT);
+    }
+  }
+
+  //Gets a word from the API and instanciates it globally.
+  private getWord(): void {
+    this.wordService.getWord(+this.wordID).subscribe(
+      response => {
+        this.word = new Word(response);
+      }, 
+      err => console.error(err));
+  }
+
+  //Gets all the wordSigns of a word and instanciates the array of video URLs globally.
+  //TODO: Create count versions route in API.
+  private createVideoURLs(): void {
+    const version: string[] = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J'];
+    const URL: string = "https://storage.googleapis.com/video.handsapp.org/LSM/words/";
+    this.wordSignService.getWordSigns(+this.wordID).subscribe(
+      response => {
+        this.videos = new Array(response.length);
+        for (let i = 0; i < response.length; i++) {
+          this.videos[i] = URL + this.wordID + '-' + version[i] + '.mp4';
+        }
+        this.ready = true;    
+      }, 
+      err => console.error(err));
+  }
+
+  //Sets the playback speed of a video.
+  //TODO: Fix method to work with videos array.
   public setPlaybackSpeed(speed: number){
     switch (speed) {
+      case 0:
+        if (this.vid.playbackRate) {
+          this.vid.playbackRate = 0;
+        } else if (this.vid.webkitPlaybackRate) {
+          this.vid.webkitPlaybackRate = 0;
+        } else if (this.vid.msPlaybackRate) {
+          this.vid.msPlaybackRate = 0;
+        }
+        break;
+
       case 0.5:
         if (this.vid.playbackRate) {
           this.vid.playbackRate = 0.5;
@@ -138,6 +190,8 @@ export class WordComponent implements OnInit {
     }
   }
 
+  //Opens a video fullscreen.
+  //TODO: Fix method to work with videos array.
   public openFullscreen() {
     if (this.vid.requestFullscreen) {
       this.vid.requestFullscreen();
@@ -148,8 +202,9 @@ export class WordComponent implements OnInit {
     }
   }
   
-  public navigate(page: String): void {
-    this.router.navigate([page]);
+  // navigate redirects faster through router with id & txt parameters.
+  public navigate(page: String, locP: string[], idP: String, txtP: String): void {
+    this.router.navigate([page], {queryParams: {loc: locP[0] + "_" + locP[1] + "_" + locP[2], id: idP, txt: txtP}});
   }
 
   get wordId() { return (this.word && this.word.wordID) ? this.word.wordID : null }
