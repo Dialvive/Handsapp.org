4070ca0736bb5a69f8c711f1472250bcf81b793d
dialvive <dialvive@protonmail.com>
2021-03-23 17:05:47 -0600

    Add Search functionality to results page

---
+++ SignLanguage/src/app/_services/search.service.ts
@@ -0,0 +1,21 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Search, WordSearchResult } from '../_models/search';
+
+const URI: string = "https://api.handsapp.org/v1/search/"
+
+@Injectable({
+  providedIn: 'root'
+})
+export class SearchService {
+
+  constructor(
+    private http: HttpClient
+    ) { }
+
+  public searchWords(q: string, limit: number): Observable<WordSearchResult> {
+    var search: Search = new Search(q, limit);
+    return this.http.post<WordSearchResult>(URI+"words", search)
+  }
+}
