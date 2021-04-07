9aef2d6e79568125b37245e2b8ca305fbd2fd7f6
tokumago <marco15@ciencias.unam.mx>
2021-04-07 18:45:53 -0500

    Translate footer & searchbar

--- SignLanguage/src/app/_components/search/search.component.ts
+++ SignLanguage/src/app/_components/search/search.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { AppComponent } from '../../app.component';
 
 @Component({
   selector: 'app-search',
@@ -6,8 +7,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./search.component.css']
 })
 export class SearchComponent implements OnInit {
-
-  constructor() { }
+  public strWord: string[] = ["Suche nach Wörtern", "Buscar palabras", "Search words", "Rechercher des mots", "Cerca le Parole", "Procure palavras"];
+  public strPhrase: string[] = ["Suchphrasen", "Buscar frases", "Search phrases", "Rechercher des phrases", "Frasi di ricerca", "Pesquisar frases"];
+  public strSearch: string[] = ["Suche", "Buscar", "Search", "Chercher", "Ricerca", "Procurar"];
+  constructor(public appComponent: AppComponent) { }
 
   ngOnInit(): void {
   }
