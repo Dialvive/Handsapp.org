fef7e9f8483ba0e213e87e3b0fd501818e980584
tokumago <marco15@ciencias.unam.mx>
2021-04-07 18:45:53 -0500

    Translate footer & searchbar

--- SignLanguage/src/app/_components/header/header.component.ts
+++ SignLanguage/src/app/_components/header/header.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { AppComponent } from '../../app.component';
 
 @Component({
   selector: 'app-header',
@@ -7,10 +8,21 @@ import { Router } from '@angular/router';
   styleUrls: ['./header.component.css']
 })
 export class HeaderComponent implements OnInit {
-
+  public strWord: string[] = ["Wörter", "Palabras", "Words", "Mots", "Parole", "Palavras"];
+  public strPhrase: string[] = ["Sätze", "Frases", "Phrases", "Phrases", "Frasi", "Frases"];
+  public strTool: string[] = ["Werkzeuge", "Herramientas", "Tools", "Outils", "Utensili", "Ferramentas"];
+  public strAccount: string[] = ["Rechnung", "Cuenta", "Account", "Account", "Compte", "Conta"];
+  public strEmail : string[] = ["Email", "Correo Elecrónico", "Email", "E-mail","E-mail", "O email"];
+  public strPsw: string[] = ["Passwort", "Contraseña", "Password", "Mot de passe", "Palora d'ordine", "Senha" ];
+  public strComSoon : string[] = ["kommt bald","Próximamente", "Coming soon", "Bientôt disponible", "Prossimamente", "Em breve"];
+  public strLogIn : string[] = ["Einloggen", "Inicia Sesión", "Log in", "Commencer la session", "Accesso", "Iniciar sessão" ];
+  public strRemDat : string[] = ["Erinnere dich an meine Daten", "Recuerda mis datos", "Remember my data", "Souviens-toi de mes données", "Ricorda i miei dati", "Lembre-se de meus dados "];
+  public strFistTime : string[] = ["Erstes Mal? Anmelden", "¿Primera vez? Registrate", "First time?", "Première fois?  S'inscrire", "Primera volta? Iscriviti", "Primeira vez? Catasto-se "];
+  public strForgotPsw : string[] = ["Ich habe mein Passwort vergessen", "Olvidé mi contraseña", "I forgot my password", "J'ai oublié mon mot de passe", "Ho dimenticato la mia password", "Esqueci minha senha" ];
+  
   txt: String;
 
-  constructor(private router: Router) {
+  constructor(private router: Router, public appComponent: AppComponent) {
     this.txt = "";
   }
   
