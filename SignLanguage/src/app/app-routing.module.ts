5123f17b08e1e69dd9629e01ac6a883dde1154e8
dialvive <dialvive@protonmail.com>
2021-04-13 20:57:39 -0500

    Added components to develop

--- SignLanguage/src/app/app-routing.module.ts
+++ SignLanguage/src/app/app-routing.module.ts
@@ -8,6 +8,8 @@ import { AboutUsComponent } from './_components/about-us/about-us.component';
 import { ProductsComponent } from './_components/products/products.component';
 import { TermsComponent } from './_components/terms/terms.component';
 import { PrivacyComponent } from './_components/privacy/privacy.component'
+import { ProximamenteComponent } from './_components/proximamente/proximamente.component';
+import { DonationsComponent } from './_components/donations/donations.component';
 
 const routes: Routes = [
   { path: '', component:  SearchComponent },
@@ -17,6 +19,9 @@ const routes: Routes = [
   { path: 'about-us', component: AboutUsComponent},
   { path: 'products', component: ProductsComponent},
   { path: 'terms', component: TermsComponent},
+  { path: 'privacy', component: PrivacyComponent},
+  { path: 'coming-soon', component: ProximamenteComponent},
+  { path: 'donors', component: DonationsComponent},
   { path: 'privacy', component: PrivacyComponent},
 ];
 
