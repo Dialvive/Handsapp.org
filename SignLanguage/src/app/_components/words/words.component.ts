aed898f4bfaff006644dfad12931c5d081b339f6
tokumago <marco15@ciencias.unam.mx>
2021-04-14 19:03:23 -0500

    checkpoint accordion first stage working

--- SignLanguage/src/app/_components/words/words.component.ts
+++ SignLanguage/src/app/_components/words/words.component.ts
@@ -1,5 +1,10 @@
 import { Component, OnInit } from '@angular/core';
-import { Router } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
+import { AppComponent } from 'src/app/app.component';
+import { WordCategory } from 'src/app/_models/wordCategory';
+import { WordCategoryService } from 'src/app/_services/word-category/word-category.service';
+import { WordService } from 'src/app/_services/word/word.service';
+import { CategoriesComponent } from '../categories/categories.component';
 
 @Component({
   selector: 'app-words',
@@ -7,10 +12,18 @@ import { Router } from '@angular/router';
   styleUrls: ['./words.component.css']
 })
 export class WordsComponent implements OnInit {
+  public titleStr: string[] = ["Thematischer Wortindex","Índice temático de palabras", "Thematic index of words", "Index thématique des mots", "Indice tematico delle parole", "Índice temático de palavras"  ];
+  public strCat: WordCategory[] | any;
+  public categories: string[] | any;
 
-  constructor(private router: Router) { }
+  constructor(private router: Router,
+    private wordService: WordService,
+    public appComponent: AppComponent,
+    public wordCategoryService: WordCategoryService) { }
 
   ngOnInit(): void {
+    this.appComponent.getLocale();
+    this.getWordCategories();
   }
 
   // navigate redirects faster through router
@@ -23,5 +36,32 @@ export class WordsComponent implements OnInit {
     this.router.navigate([page], {queryParams: {id: idP, txt: txtP}});
   }
   
+  //Gets all categories from the API and instanciates it globally.
+  private getWordCategories(): void {
+    this.wordCategoryService.getWordCategories().subscribe(
+      response => {
+        this.strCat = response;
+        this.sortCategories();
+        this.fillCategories()
+      }, 
+      err => this.appComponent.navigate("/404"));
+  }
+
+  //Sort strCat categories by name_es 
+  private sortCategories(){
+    this.strCat.sort(function(a:any, b:any){
+      if(a.name_es < b.name_es) { return -1; }
+      if(a.name_es > b.name_es) { return 1; }
+      return 0;
+    })
+  }
+
+  //Create a second array of all languages of the categories
+  private fillCategories(){
+    this.categories = new Array<String[]>(this.strCat.length)
+    for (let i = 0; i < this.strCat.length; i++) {
+      this.categories[i] = new WordCategory(this.strCat[i]).getNames();
+    }
+  }
 
 }
