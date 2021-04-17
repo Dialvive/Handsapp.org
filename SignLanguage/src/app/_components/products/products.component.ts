6350d77929d0f9fb22ba1903ce438785a54ee46d
dialvive <dialvive@protonmail.com>
2021-04-16 20:09:15 -0500

    Add products

--- SignLanguage/src/app/_components/products/products.component.ts
+++ SignLanguage/src/app/_components/products/products.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { AppComponent } from 'src/app/app.component';
 
 @Component({
   selector: 'app-products',
@@ -7,7 +8,9 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProductsComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    public appComponent: AppComponent,
+  ) { }
 
   ngOnInit(): void {
   }
