1335d4d8cae112fae684bf40518ff12133e69ee7
dialvive <dialvive@protonmail.com>
2021-05-06 16:24:35 -0500

    Add Terms

--- SignLanguage/src/app/_components/terms/terms.component.ts
+++ SignLanguage/src/app/_components/terms/terms.component.ts
@@ -8,7 +8,21 @@ import { AppComponent } from 'src/app/app.component';
 })
 export class TermsComponent implements OnInit {
 
-  public strTitle: string[] = ["Geschäftsbedingungen", "Términos y condiciones", "Terms and conditions", "Termes et conditions", "Termini e condizioni", "Termos e condições"];
+  public strTitle: string[] = 
+  ["Allgemeine Geschäftsbedingungen für alle Anwendungen anwendbar, Produkte und Dienstleistungen im Zusammenhang mit dem HandsApp Platform",
+  "Términos y Condiciones aplicables a todas las Aplicaciones, Productos y Servicios Referentes a la Plataforma HandsApp",
+  "Terms and Conditions applicable to all Applications, Products and Services Relating to the HandsApp Platform",
+  "Termes et conditions applicables à toutes les applications, les produits et services liés à la plate-forme HandsApp",
+  "Termini e condizioni applicabili a tutte le applicazioni, prodotti e servizi relativi alla piattaforma HandsApp ",
+  "Termos e Condições aplicáveis a todos os aplicativos, produtos e serviços relacionados à plataforma HandsApp"];
+  
+  public strAlert: string[] =
+  ["Im Moment haben wir dieses Dokument nur auf Spanisch.",
+  "",
+  "At the moment, we only have this document in Spanish.",
+  "Pour le moment, nous n'avons que ce document en espagnol.",
+  "Al momento, abbiamo solo questo documento in spagnolo.",
+  "No momento, temos este documento apenas em espanhol."];
 
   constructor(
     public appComponent: AppComponent,
