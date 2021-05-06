9cc8022fa521cba423c99171fec5fcfc99919f27
dialvive <dialvive@protonmail.com>
2021-05-06 16:24:28 -0500

    Add alert

--- SignLanguage/src/app/_components/privacy/privacy.component.ts
+++ SignLanguage/src/app/_components/privacy/privacy.component.ts
@@ -9,7 +9,14 @@ import { AppComponent } from 'src/app/app.component';
 export class PrivacyComponent implements OnInit {
 
   public strTitle: string[] = ["Datenschutzbestimmungen", "Política de privacidad", "Privacy policy", "Politique de confidentialité ", "Politica sulla piservatezza ", "Política de privacidade"];
-
+  public strAlert: string[] =
+  ["Im Moment haben wir dieses Dokument nur auf Spanisch.",
+  "",
+  "At the moment, we only have this document in Spanish.",
+  "Pour le moment, nous n'avons que ce document en espagnol.",
+  "Al momento, abbiamo solo questo documento in spagnolo.",
+  "No momento, temos este documento apenas em espanhol."];
+  
   constructor(
     public appComponent: AppComponent,
   ) { }
