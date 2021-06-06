38c04b94bc809b288dd1dbbc4077ac0dfcde8bcc
dialvive <dialvive@protonmail.com>
2021-06-06 17:03:54 -0500

    ADD init register-continue

---
+++ SignLanguage/src/app/_components/register-continue/register-continue.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-register-continue',
+  templateUrl: './register-continue.component.html',
+  styleUrls: ['./register-continue.component.css']
+})
+export class RegisterContinueComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
